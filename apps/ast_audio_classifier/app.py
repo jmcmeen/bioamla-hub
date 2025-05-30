8743c016654b89c14f46b075705941f803cc70e9
John McMeen <johnmcmeen@gmail.com>
2025-05-29 23:02:21 -0400

    Update app.py

--- apps/ast_audio_classifier/app.py
+++ apps/ast_audio_classifier/app.py
@@ -3,17 +3,17 @@ import streamlit as st
 from apps.torch_ast import wave_file_ast_inference
 
 # Streamlit configuration
-st.set_page_config(page_title="Audio Classifier", layout="wide")
+st.set_page_config(page_title="Environmental Audio Classifier", layout="wide")
 
 # Set page title
-st.title("Audio Classifier using Audio Spectrogram Transformer")
+st.title("Environmental Audio Classifier using Audio Spectrogram Transformer")
 
 # Write description
-st.write("This app uses the following pretrained model: https://huggingface.co/MIT/ast-finetuned-audioset-10-10-0.4593")
+st.write("This app uses the following pretrained model: https://huggingface.co/bioamla/ast-esc50")
 
 # Create upload widget for image
 uploaded_file = st.file_uploader("Choose a wave file, please!", type=["wav"])
-model = "MIT/ast-finetuned-audioset-10-10-0.4593"
+model = "bioamla/ast-esc50"
 resample_freq = 16000
 
 # Make prediction
